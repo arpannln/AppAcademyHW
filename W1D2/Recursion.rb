49dff19b98ccf1030eb3f41b953903f807f2a659
Arpan NLN <arpannln@ucla.edu>
2017-12-09 10:07:58 -0800

    added some previous hw

---
+++ W1D2/Recursion.rb
@@ -0,0 +1,67 @@
+def sum_to(n)
+  if n < 0
+    nil
+  elsif n == 1
+    1
+  else
+    n + sum_to(n - 1)
+  end
+end
+
+p sum_to(5) # => returns 15
+p sum_to(1)  # => returns 1
+p sum_to(9)  # => returns 45
+p sum_to(-8) # => returns nil
+
+def add_numbers(arr)
+  if arr.empty?
+    nil
+  elsif arr.length == 1
+    arr[0]
+  else
+    arr.pop + add_numbers(arr)
+  end
+end
+
+p add_numbers([1,2,3,4]) # => returns 10
+p add_numbers([3]) # => returns 3
+p add_numbers([-80,34,7]) # => returns -39
+p add_numbers([]) # => returns nil
+
+def gamma_fnc(n)
+  if n == 0
+    nil
+  elsif n == 1
+    1
+  else
+    (n - 1) * gamma_fnc(n - 1)
+  end
+end
+
+p gamma_fnc(0)  # => returns nil
+p gamma_fnc(1)  # => returns 1
+p gamma_fnc(4)  # => returns 6
+p gamma_fnc(8)  # => returns 5040
+
+def ice_cream_shop(arr, flavor)
+  return false if arr.empty?
+  arr.pop == flavor || ice_cream_shop(arr, flavor)
+end
+
+p ice_cream_shop(['vanilla', 'strawberry'], 'blue moon')  # => returns false
+p ice_cream_shop(['pistachio', 'green tea', 'chocolate', 'mint chip'], 'green tea')  # => returns true
+p ice_cream_shop(['cookies n cream', 'blue moon', 'superman', 'honey lavender', 'sea salt caramel'], 'pistachio')  # => returns false
+p ice_cream_shop(['moose tracks'], 'moose tracks')  # => returns true
+p ice_cream_shop([], 'honey lavender')  # => returns false
+
+def reverse(str)
+  return "" if str == ""
+  str[-1] + reverse(str[0...-1])
+end
+
+p reverse("house") # => "esuoh"
+p reverse("dog") # => "god"
+p reverse("atom") # => "mota"
+p reverse("q") # => "q"
+p reverse("id") # => "di"
+p reverse("") # => ""
