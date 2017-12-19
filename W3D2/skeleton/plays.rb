0722af7ec66a60d89cd01c129d79887ea9dfc1fb
Arpan NLN <arpannln@ucla.edu>
2017-12-19 08:27:45 -0800

    made some changes to hw after checking out solutions

--- W3D2/skeleton/plays.rb
+++ W3D2/skeleton/plays.rb
@@ -16,7 +16,7 @@ class Play
 
   def self.all
     data = PlayDBConnection.instance.execute("SELECT * FROM plays")
-    data.map { |datum| Play.new(datum) }
+    data.map { |el| Play.new(el) }
   end
 
   def initialize(options)
@@ -25,7 +25,37 @@ class Play
     @year = options['year']
     @playwright_id = options['playwright_id']
   end
+  
+  def self.find_by_title(title)
+   play = PlayDBConnection.instance.execute(<<-SQL, title)
+     SELECT
+       *
+     FROM
+       plays
+     WHERE
+       title = ?
+   SQL
+   return nil unless play.length > 0
 
+   Play.new(play.first)
+  end
+  
+  def self.find_by_playwright(name)
+    playwright = Playwright.find_by_name(name)
+    raise "not found in DB" if !playwright
+
+    plays = PlayDBConnection.instance.execute(<<-SQL, playwright.id)
+      SELECT
+        *
+      FROM
+        plays
+      WHERE
+        playwright_id = ?
+    SQL
+
+    plays.map { |play| Play.new(play) }
+  end
+ 
   def create
     raise "already in database" if @id
     PlayDBConnection.instance.execute(<<-SQL, @title, @year, @playwright_id)
@@ -38,7 +68,7 @@ class Play
   end
 
   def update
-    raise "not in database" unless @id
+    raise "not in database" if !@id
     PlayDBConnection.instance.execute(<<-SQL, @title, @year, @playwright_id, @id)
       UPDATE
         plays
@@ -65,8 +95,60 @@ class Playwright
   
   def self.all
     data = PlayDBConnection.instance.execute("SELECT * FROM playwrights")
-    data.map { |notsure| Playwright.new(notsure) }
+    data.map { |el| Playwright.new(el) }
   end
+  
+
+ 
+ def initialize(options)
+   @id = options['id']
+   @title = options['title']
+   @year = options['year']
+   @playwright_id = options['playwright_id']
+ end
+ 
+ 
+ def self.find_by_playwright(name)
+   playwright = Playwright.find_by_name(name)
+   raise "not found in DB" unless playwright
+
+   plays = PlayDBConnection.instance.execute(<<-SQL, playwright.id)
+     SELECT
+       *
+     FROM
+       plays
+     WHERE
+       playwright_id = ?
+   SQL
+
+   plays.map { |play| Play.new(play) }
+ end
+ 
+ def create
+    raise "already in database" if @id
+    PlayDBConnection.instance.execute(<<-SQL, @title, @year, @playwright_id)
+      INSERT INTO
+        plays (title, year, playwright_id)
+      VALUES
+        (?, ?, ?)
+    SQL
+    @id = PlayDBConnection.instance.last_insert_row_id
+  end
+  
+  def update
+      raise " not in database" if !@id
+      PlayDBConnection.instance.execute(<<-SQL, @title, @year, @playwright_id, @id)
+        UPDATE
+          plays
+        SET
+          title = ?, year = ?, playwright_id = ?
+        WHERE
+          id = ?
+      SQL
+    end
+
+ 
+
 
   def self.find_by_name(name)
     person = PlayDBConnection.instance.execute(<<-SQL, name)
@@ -89,24 +171,41 @@ class Playwright
     @birth_year = options['birth_year']
   end
 
-  def create
-    raise "#{self} already in database" if @id
+  def create 
+    raise "already in database" if @id
     PlayDBConnection.instance.execute(<<-SQL, @name, @birth_year)
       INSERT INTO
         playwrights (name, birth_year)
-      VALUES
-        (?, ?)
+      VALUES 
+        (?, ?) 
     SQL
     @id = PlayDBConnection.instance.last_insert_row_id
   end
 
-  def update
-    raise "not in database" unless @id
-  
+  def update #good example on how to update database 
+    raise "not in database" if !@id
+    PlayDBConnection.instance.execute(<<-SQL, @name, @birth_year, @id)
+      UPDATE 
+        playwrights 
+      SET 
+        name = ?, birth_year = ? 
+      WHERE 
+        id = ? 
+    SQL
   end
 
-  def get_plays
-  
+  def get_plays #didn't get this 
+    # raise "not in database" if !@id
+    # plays = PlayDBConnection.instance.execute(<<-SQL, @id)
+    #   SELECT 
+    #     * 
+    #   FROM 
+    #     plays 
+    #   WHERE 
+    #     playwright_id = ?
+    # 
+    #   SQL 
+    #   plays.map { |play| Play.new(play)}
   end
 
 end
